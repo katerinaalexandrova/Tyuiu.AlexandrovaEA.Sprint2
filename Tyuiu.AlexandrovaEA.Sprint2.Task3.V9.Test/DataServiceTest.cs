c8a2a8b79036e0b24b899019b953e8981f56b84b
tyuiu.alexandrovaea@outlook.com <tyuiu.alexandrovaea@outlook.com>
2023-10-17 17:15:21 +0500

    Task 3

---
+++ Tyuiu.AlexandrovaEA.Sprint2.Task3.V9.Test/DataServiceTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.AlexandrovaEA.Sprint2.Task3.V9.Lib;
+
+namespace Tyuiu.AlexandrovaEA.Sprint2.Task3.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            Assert.AreEqual(-2.667, Math.Round(res, 3));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            Assert.AreEqual(1, Math.Round(res, 3));
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            DataService ds = new DataService();
+            double x = -11;
+            double res = ds.Calculate(x);//
+            Assert.AreEqual(1.067, Math.Round(res, 3));
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            DataService ds = new DataService();
+            double x = 12;
+            double res = ds.Calculate(x);
+            Assert.AreEqual(7377451514.118, Math.Round(res, 3));
+        }
+    }
+}
---
+++ Tyuiu.AlexandrovaEA.Sprint2.Task3.V9/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AlexandrovaEA.Sprint2.Task3.V9.Lib;
+
+namespace Tyuiu.AlexandrovaEA.Sprint2.Task3.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Александрова Е. А. | ИСПБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: АЛГОРИТМЫ РАЗВЕТВЛЯЮЩЕЙСЯ СТРУКТУРЫ                               *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнил: Александрова Е. А. | ИСПБ-23-1                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с    *");
+            Console.WriteLine("* использованием вложенных оператор if-else, где пользователь вводит      *");
+            Console.WriteLine("* значение переменной X с клавиатуры.Округлить полученное значение до трех*");
+            Console.WriteLine("* знаков после запятой;                                                   *");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("X = ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x));
+
+
+            Console.ReadKey();
+        }
+    }
+}
